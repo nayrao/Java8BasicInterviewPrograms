7cd974e51946a0c9f8b8c1c0d068faa4703c2485
RajaRao <rajarao1284@gmail.com>
2022-07-27 13:21:09 +0530

    code added for map object

--- src/raja/nayapamu/prime/ForEachDemo.java
+++ src/raja/nayapamu/prime/ForEachDemo.java
@@ -1,7 +1,9 @@
 package raja.nayapamu.prime;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ForEachDemo {
 	
@@ -20,6 +22,16 @@ public class ForEachDemo {
 		
 		//using java8
 		listOfName.stream().forEach(t->System.out.println("Using Java8::"+t));
+		
+		Map<Integer,String> mapObject=new HashMap<>();
+		mapObject.put(1,"Raja");
+		mapObject.put(2, "John");
+		mapObject.put(3, "Jacob");
+		mapObject.put(4, "Daniel");
+		
+		mapObject.forEach((key,value)->System.out.println("key::"+key +"Value::"+value));
+		
+		mapObject.entrySet().stream().forEach(obje->System.out.println("Using Java8::"+obje));
 		
 	}
 
