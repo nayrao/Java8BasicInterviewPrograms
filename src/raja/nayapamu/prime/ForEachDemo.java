b6a207f6a061fe40e69493492204eb9ef998e694
RajaRao <rajarao1284@gmail.com>
2022-07-27 13:50:12 +0530

    adding filter logic

--- src/raja/nayapamu/prime/ForEachDemo.java
+++ src/raja/nayapamu/prime/ForEachDemo.java
@@ -14,14 +14,28 @@ public class ForEachDemo {
 		listOfName.add("nayapamu");
 		listOfName.add("john");
 		listOfName.add("peter");
+		listOfName.add("jacob");
 		
-		//normal foreach 
+		/*
+		 * //normal foreach for(String s:listOfName) {
+		 * System.out.println("Normal ForEachLoop::"+s); }
+		 */
+		
+		/*
+		 * //using java8
+		 * listOfName.stream().forEach(t->System.out.println("Using Java8::"+t));
+		 */	
+		
+		//Write a Logic using Java8 Filter
 		for(String s:listOfName) {
-			System.out.println("Normal ForEachLoop::"+s);
+			if(s.startsWith("R")) {
+				System.out.println("Just normal condition::"+s);
+			}
 		}
 		
-		//using java8
-		listOfName.stream().forEach(t->System.out.println("Using Java8::"+t));
+		//using java8 filter
+		
+		listOfName.stream().filter(s->s.startsWith("j")).forEach(s->System.out.println("Using filter()::"+s));
 		
 		Map<Integer,String> mapObject=new HashMap<>();
 		mapObject.put(1,"Raja");
@@ -29,9 +43,15 @@ public class ForEachDemo {
 		mapObject.put(3, "Jacob");
 		mapObject.put(4, "Daniel");
 		
-		mapObject.forEach((key,value)->System.out.println("key::"+key +"Value::"+value));
+		/*
+		 * mapObject.forEach((key,value)->System.out.println("key::"+key
+		 * +"Value::"+value));
+		 * 
+		 * mapObject.entrySet().stream().forEach(obje->System.out.
+		 * println("Using Java8::"+obje));
+		 */
 		
-		mapObject.entrySet().stream().forEach(obje->System.out.println("Using Java8::"+obje));
+		mapObject.entrySet().stream().filter(key->key.getValue().contains("J")).forEach(obje->System.out.println("Using Java8::"+obje));
 		
 	}
 
